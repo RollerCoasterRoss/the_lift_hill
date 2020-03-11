2db4ec500dbb4c343882ea50fb38efc3ad1da932
RollerCoasterRoss <rsw3020@gmail.com>
2020-03-11 11:48:06 -0500

    Added in routes and uncommented protect from forgery

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
+    post '/users' => 'users#create'
+    get '/users/:id' => 'users#show'
+    patch '/users/:id' => 'users#update'
+    delete '/users/:id' => 'users#destroy'
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+    get '/attraction_reviews' => 'attraction_reviews#index'
+    post '/attraction_reviews' => 'attraction_reviews#create'
+    get '/attraction_reviews/:id' => 'attraction_reviews#show'
+    patch '/attraction_reviews/:id' => 'attraction_reviews#update'
+    delete '/attraction_reviews/:id' => 'attraction_reviews#destroy'
+
+    get '/park_reviews' => 'park_reviews#index'
+    post '/park_reviews' => 'park_reviews#create'
+    get '/park_reviews/:id' => 'park_reviews#show'
+    patch '/park_reviews/:id' => 'park_reviews#update'
+    delete '/park_reviews/:id' => 'park_reviews#destroy'
+
+    get '/attractions' => 'attractions#index'
+    get '/attractions/:id' => 'attractions#show'
+
+    get '/images' => 'images#index'
+    post '/images' => 'images#create'
+    get '/images/:id' => 'images#show'
+    delete '/images/:id' => 'images#destroy'
+  end
 end
